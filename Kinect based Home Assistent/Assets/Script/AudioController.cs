304220beae0fefbcff7ccdb94019ca44b81c9968
jjeongone <jjeongone@gmail.com>
2022-11-20 02:41:18 +0900

    Feat : Implement audio controller for TV

---
+++ Kinect based Home Assistent/Assets/Script/AudioController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource TelevisionAudio;
+    [SerializeField] private AudioClip[] clip;
+    private float volumeParameter = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TelevisionAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAudio()
+    {
+        TelevisionAudio.Play();
+        Debug.Log("play");
+    }
+
+    public void PauseAudio()
+    {
+        TelevisionAudio.Pause();
+        Debug.Log("pause");
+    }
+
+    public void StopAudio()
+    {
+        TelevisionAudio.Stop();
+        Debug.Log("stop");
+    }
+
+    public void VolumeUp()
+    {
+        TelevisionAudio.volume += volumeParameter;
+        Debug.Log("volume up");
+    }
+
+    public void VolumeDown()
+    {
+        TelevisionAudio.volume -= volumeParameter;
+        Debug.Log("volume down");
+    }
+}
